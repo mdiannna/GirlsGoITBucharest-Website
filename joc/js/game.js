7d87a214bea6722d60a296351f8cbd85921e6fa4
mdiannna <marusicdiana@gmail.com>
2017-12-25 22:06:43 +0200

    code refactor

--- joc/js/game.js
+++ joc/js/game.js
@@ -1,19 +1,20 @@
-function moveButterfly() {
-	document.getElementById("sprite").style.left = "100px";
-	document.getElementById("sprite").style.top = "100px";
-}
+// TODO: constants file separately
+var STEP = 10;
+
 
 var butterflyPositionTop;
 var butterflyPositionLeft;
-var STEP = 10;
+
 
 function init() {
 	butterflyPositionTop = 0;
 	butterflyPositionLeft = 0;
 }
-init();
 
-document.onkeydown = checkKey;
+function moveSprite() {
+	document.getElementById("sprite").style.left = "100px";
+	document.getElementById("sprite").style.top = "100px";
+}
 
 
 function checkKey(e) {
@@ -39,22 +40,18 @@ function checkKey(e) {
 
     document.getElementById("sprite").style.top = butterflyPositionTop + "px";
     document.getElementById("sprite").style.left = butterflyPositionLeft + "px";
-    // document.getElementById("sprite").style.right = butterflyPositionright + "px";
-    // document.getElementById("sprite").style.top = butterflyPositionTop + "px";
     fallCodeBlock();
 }
 
 
-function startFalling(myObject) {
-	// 	myObject.style.top +=  i*10 + "px";
-	// 	myObject.style.left +=  i*10 + "px";
-
-}
 function fallCodeBlock() {
 	var codeBlocks = document.querySelectorAll(".codeBlock");
 	for (var i=0; i<codeBlocks.length; i++) {
-		// startFalling(codeBlocks[i]);
 		codeBlocks[i].style.top += 150 * i + "px";
 		codeBlocks[i].style.left += 200 * i + "px";
 	}
 }
+
+
+init();
+document.onkeydown = checkKey;
